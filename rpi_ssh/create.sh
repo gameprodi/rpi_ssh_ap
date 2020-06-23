e7055a758821018d229c17946c366d9430dfca04
Andrew <46055496+gameprodi@users.noreply.github.com>
2020-06-23 19:39:31 +0300

    Delete create.sh

--- rpi_ssh/create.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo macchanger -m f8:d1:11:f3:d4:c2 <your wifi-interface> 
-sudo systemctl start ssh 
-sudo create_ap -n <your wifi-interface> <Name of AP> <password>  --no-virt -c 1
-#Example
-#sudo macchanger -m f8:d1:11:f3:d4:c2 wlan0 
-#sudo systemctl start ssh 
-#sudo create_ap -n wlan0 RaspberryPi raspberrypi  --no-virt -c 1
